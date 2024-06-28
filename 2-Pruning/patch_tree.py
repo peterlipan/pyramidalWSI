af94256a1fcc99c960c68bf87ba5d755e0037cf6
johnP-Li <847041172@qq.com>
2024-06-28 17:47:53 +0800

    Initial commit

--- 2-Pruning/patch_tree.py
+++ 2-Pruning/patch_tree.py
@@ -93,9 +93,10 @@ class PatchTree:
 
 class LevelPatchDataset(Dataset):
 
-    def __init__(self, tree: MyNode, level: int):
+    def __init__(self, tree: MyNode, level: int, patch_size: int):
         self.tree = tree
         self.level = level
+        self.patch_size = patch_size
         self.node_list = [node for node in LevelOrderIter(tree.root) if node.level == level]
     
     def __len__(self):
@@ -109,9 +110,9 @@ class LevelPatchDataset(Dataset):
 
         return patch, filename, idx
 
-    @staticmethod
-    def transform(images):
+    def transform(self, images):
         transform = transforms.Compose([
+            transforms.Resize((self.patch_size, self.patch_size)),
             transforms.ToTensor()
         ])
         return transform(images)
